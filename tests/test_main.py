faf73ea520f72e9f1c9be5df54042da47b443088
robcamgar <roberto.de.la.camara.garcia@gmail.com>
2025-01-09 15:32:37 +0100

    kjhkh

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,6 +5,7 @@ from fastapi.testclient import TestClient
 from src.main import app
 import os
 import shutil
+import pandas as pd
 
 client = TestClient(app)
 
@@ -15,19 +16,16 @@ def setup_synthetic_data():
     yield
     shutil.rmtree(synthetic_dir)
 
+
 def test_load_network_from_file(setup_synthetic_data):
     """Test the /loadNetwork endpoint with a file."""
-    file_path = os.path.join(os.path.dirname(__file__), "../data/synthetic/tests/synthetic_test_network.csv")
     
-    # Create synthetic test network CSV file
-    with open(file_path, "w") as file:
-        file.write("origin,destination,weight\n")
-        file.write("1,48,50\n")
-        file.write("48,15,30\n")
-        file.write("15,29,40\n")
-        file.write("29,3,49\n")
-            
-    with open(file_path, "rb") as file:
+    network_data = {'origin': ['1', '48', '15', '29'], 'destination': ['48', '15', '29', '3'], 'weight': [50, 30, 40,49]}
+    network_df = pd.DataFrame(network_data)
+    adjacency_matrix_file = '../data/synthetic/tests/test_network.csv'
+    network_df.to_csv(adjacency_matrix_file, index=False)
+    
+    with open(adjacency_matrix_file, "rb") as file:
         response = client.post("/loadNetwork", files={"file": file})
         
     assert response.status_code == 200
